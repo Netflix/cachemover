3997770095c2eea79b876d1754f7e4da6af54fb0
Tharanga Gamaethige <tgamaethige@netflix.com>
2020-06-03 20:57:48 -0700

    Allocated pool buffers as one big chunk and divided among individual buffers. Also made get buffer to work on the tail of buffer pools, so same memory areas are reused.

--- utils/mem_mgr.cc
+++ utils/mem_mgr.cc
@@ -13,14 +13,19 @@ MemoryManager::MemoryManager(uint64_t chunk_size, int num_chunks)
 
 Status MemoryManager::PreallocateChunks() {
   std::lock_guard<std::mutex> lock(list_mutex_);
+  std::cout << "Chunk size is " << chunk_size_ << std::endl;
+
+  uint8_t* main_buff = static_cast<uint8_t*>(malloc(chunk_size_ * num_chunks_));
+
+  if (main_buff == nullptr) {
+      return Status::OutOfMemoryError("Could not pre-allocate chunks");
+  }
+
 
   for (int i = 0; i < num_chunks_; ++i) {
-    uint8_t *buf = static_cast<uint8_t*>(malloc(chunk_size_));
-    if (buf == nullptr) {
-      return Status::OutOfMemoryError("Could not pre-allocate chunks");
-    }
-
+    uint8_t *buf = main_buff;
     free_buffers_.push_back(buf);
+    main_buff += chunk_size_;
   }
 
   return Status::OK();
@@ -33,15 +38,16 @@ uint8_t* MemoryManager::GetBuffer() {
 
   if (free_buffers_.empty()) return nullptr;
 
-  uint8_t *buf = free_buffers_.front();
-  free_buffers_.pop_front();
-
+  uint8_t *buf = free_buffers_.back();
+  free_buffers_.pop_back();
+  std::cout << "Getting " <<  free_buffers_.size() << std::endl;
   return buf;
 }
 
 void MemoryManager::ReturnBuffer(uint8_t *buf) {
   std::lock_guard<std::mutex> lock(list_mutex_);
   free_buffers_.push_back(buf);
+  std::cout << "Returned " <<  free_buffers_.size() << std::endl;
 }
 
 } // namespace memcachedumper
