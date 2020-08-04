ed256dbd9f68bd9210b72fe7ed4ff6bcc1d4981b
Prudhviraj Karumanchi <pkarumanchi@netflix.com>
2020-08-04 12:22:50 -0700

    resolving merge issues

--- dumper-main.cc
+++ dumper-main.cc
@@ -93,12 +93,6 @@ int ParseCLIOptions(int argc, char **argv, memcachedumper::DumperOptions& opts) 
         exit(-1);
   }
 
-  if (num_threads * 2 * bufsize > memlimit) {
-        std::cout << "Total memory provisioned is not enough for the total threads and buffers !" << std::endl;
-        LOG_ERROR("Received fatal error: Total memory provisioned is not enough for the total threads and buffers ! ");
-        exit(-1);
-  }
-
   return 0;
 }
 
